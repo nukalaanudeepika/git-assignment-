56406ea2f075833f130d927da382ee6cdde835bc
nukalaanudeepika <nukalaanudeepika@gmail.com>
2024-04-09 12:30:37 +0530

    reverse.java

--- reverse.java
+++ reverse.java
@@ -2,7 +2,7 @@ public class ReverseNumberExample1
 {  
 public static void main(String[] args)   
 {  
-int number = 987654, reverse = 0;  
+int number = 3456, reverse = 0;  
 while(number != 0)   
 {  
 int remainder = number % 10;  
